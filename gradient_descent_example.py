35ec96d692f5429ff7c88b38a0e75fd758ebc9f5
Patrick Ryan <patr@bu.edu>
2018-10-26 10:56:19 -0400

    Update gradient_descent_example.py

--- gradient_descent_example.py
+++ gradient_descent_example.py
@@ -1,19 +1,24 @@
 
 
+# spark submit option
+# cd /home/pryan/Desktop/spark/bin/
+# spark-submit  /home/pryan/Desktop/assignment_3/gradient.py  /home/pryan/Desktop/assignment_3/taxi.csv
+
 
 from __future__ import print_function
 import sys
-import numpy as np
 from numpy import *
-from scipy import stats
-import spark
 from pyspark import SparkContext, SQLContext
-from pyspark.sql import *
+
+sc = SparkContext(appName="assignment_3")
+
+
 
 
 
 def main():
 
+    
 
     ########### TASK 1 ###############################################################################################
 
@@ -21,23 +26,33 @@ def main():
 
     ###########################################################################################################3######
 
+    
+    
+    # cleaning up data
+    lines = sc.textFile(sys.argv[1])
+
+    taxilines = lines.map(lambda x: x.split(','))
+
+    # Exception Handling  and removing wrong data lines
+    def isfloat(value):
+        try:
+            float(value)
+            return True
+        except:
+            return False
+
+    # remove lines if they don't have 16 values
+    def correctRows(p):
+        if (len(p) == 17):
+            if (isfloat(p[5]) and isfloat(p[11])):
+                if (float(p[5]) != 0 and float(p[11]) != 0):
+                    if (float(p[5]) > 0 and float(p[5]) < 100):
+                         if (float(p[11]) > 5 and float(p[11]) < 100):
+                            return p
 
 
-    sc = SparkContext(appName="assignment_3")
+    xyrdd = taxilines.filter(correctRows).map(lambda x: (float(x[5]), float(x[11])))
 
-    points = readData(sys.argv[1], 5.0, 100.0)
-    xrdd = sc.parallelize(points[0])
-    yrdd = sc.parallelize(points[1])
-    n = len(points[0])
-
-
-    #TODO: fix so it doenst need to map twice
-    # zips the two RDD's so that each item becomes a tuple of (trip distance(i), total revenue(i))
-    # xyrdd = yrdd.zipWithIndex().map(lambda x: (x[1], x[0])).join(xrdd.zipWithIndex().map(lambda y: (y[1], y[0]))).map(lambda x: x[1])
-    # xyrdd = xyrdd.map(lambda x: (x[1], x[0]))
-
-    xyrdd = xrdd.zip(yrdd)
-    # print(xyrdd.take(5))
 
 
     def calc_m_hat(xyrdd):
@@ -85,18 +100,6 @@ def main():
 
 
 
-    m_hat = (calc_m_hat(xyrdd))
-    b_hat = (calc_b_hat(xyrdd))
-
-    print("\n")
-    print("Solution 1: ")
-    print("m_hat", m_hat)
-    print("b_hat", b_hat)
-    print("\n")
-
-
-
-
 
 
     ########## Task 2  ############################################################################################
@@ -121,23 +124,6 @@ def main():
         samp_m_hat_list.append(calc_m_hat(samp_xyrdd))
         samp_b_hat_list.append(calc_b_hat(samp_xyrdd))
 
-
-
-
-    print("samp_m_hat_list: ", samp_m_hat_list)
-    print("samp_b_hat_list: ", samp_b_hat_list)
-
-    mean_m_hat = (sum(samp_m_hat_list) / len(samp_m_hat_list))
-    mean_b_hat = (sum(samp_b_hat_list) / len(samp_b_hat_list))
-
-
-
-
-    print("\n")
-    print("Solution 2: ")
-    print("mean m hat: ", mean_m_hat)
-    print("meah b hat: ", mean_b_hat)
-    print("\n")
 
 
 
@@ -166,11 +152,8 @@ def main():
         iters = 0
 
         previous_step_size = float(1)
+        points = sc.parallelize((points))
 
-        xyrdd = sc.parallelize(points.takeSample(False, 1000))
-
-        xrdd = xyrdd.map(lambda x: (x[0]))
-        yrdd = xyrdd.map(lambda x: (x[1]))
 
         m_current = float(m_current)
         b_current = float(b_current)
@@ -180,8 +163,10 @@ def main():
 
         while (previous_step_size > precision and iters < max_iterations):
 
+            xyrdd = sc.parallelize(points.takeSample(False, 1000))
 
-            y_current = xrdd.map(lambda x: (x * m_current) + b_current)
+            y_current = xyrdd.map(lambda x: (x[0] * m_current) + b_current)
+
             m_prev = m_current
 
             joined = xyrdd.zip(y_current).map(lambda x: (x[0][0], x[0][1], x[1]))   # zip as ((x, y), y_current)
@@ -200,7 +185,7 @@ def main():
             iters = iters + 1
 
 
-            if (iters % 1 == 0):
+            if (iters % 10 == 0):
                 print("Iteration: ", iters,  " b_current: ", "{0:.10f}".format(b_current), "m_current: ", "{0:.10f}".format(m_current),
                 "previous_step_size: ", "{0:.10f}".format(previous_step_size))
 
@@ -213,18 +198,17 @@ def main():
         print("Gradient_m_hat", m_current)
         print("Gradient_b_hat", b_current)
         print("\n")
-        print("Solution 3 Check: ")
-        print("Gradient_m_hat_check", calc_m_hat(xyrdd))
-        print("Gradient_b_hat_check", calc_b_hat(xyrdd))
-        print("\n")
 
 
-        return(m_current, b_current)
+        temp = list([m_current, b_current])
+        answer = sc.parallelize(temp)
+        answer.saveAsTextFile(sys.argv[2])
+
+        return (m_current, b_current)
 
 
 
-
-    batch_gradient_descent(points=xyrdd, m_current=2, b_current=2, learning_rate=0.001, max_iterations = 10000,  precision = 0.000001 )
+    batch_gradient_descent(points=xyrdd.takeSample(False, 100000), m_current=2, b_current=2, learning_rate=0.001, max_iterations = 10000,  precision = 0.000001)
 
 
 
@@ -248,7 +232,7 @@ def isfloat(value):
         return False
 
 
-def readData(fileName, lowFilter, highFilter):   # why are uppper and lower limit filters applied?
+def readData(fileName, lowFilter, highFilter):  
     x = []
     y = []
     with open(fileName) as f:
@@ -258,15 +242,11 @@ def readData(fileName, lowFilter, highFilter):   # why are uppper and lower limi
                 if (isfloat(a[5]) and isfloat(a[11])):
                     if (float(a[5]) != 0 and float(a[11]) != 0):
                         if (float(a[11]) > lowFilter and float(a[11]) < highFilter):
-                            x.append(float(a[5]))  # trip distance0
+                            x.append(float(a[5]))  # trip distance
                             y.append(float(a[11]))  # fare amount
 
-    ax = np.array(x)
-    ay = np.array(y)
 
-    return np.vstack((ax, ay))
-
-
+    return (x, y)
 
 
 
